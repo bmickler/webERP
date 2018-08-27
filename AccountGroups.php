2ccd36dd4caee5c8f1548ba0f5f51ec066789d58
bmickler <bryce@brycemickler.com>
2018-08-26 19:27:27 -0500

    minor formatting

--- AccountGroups.php
+++ AccountGroups.php
@@ -8,6 +8,7 @@
  * @copyright 2003 - 2018 webERP.org
  * @license [GNU General Public License version 2.0 (GPLv2)](https://www.gnu.org/licenses/gpl-2.0.html)
  */
+
 /**
  * Set up the environment
  */
